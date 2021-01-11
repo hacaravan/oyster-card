9ead685c24cde6665e6b03ca7ebd2ae2e5259574
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-11 15:25:14 +0000

    adding top up tests and method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,7 +4,10 @@ class Oystercard
   attr_reader :balance
 
   def initialize
-    @balance = balance
+    @balance = 0
+  end
+
+  def top_up(amount)
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,5 +3,19 @@ require 'oystercard'
 describe Oystercard do
 
   it { is_expected.to respond_to :balance }
+  it { is_expected.to respond_to(:top_up).with(1).argument }
 
-end 
+  describe '#balance' do
+    subject { Oystercard.new.balance }
+    it { is_expected.to eq 0 }
+  end
+
+  describe '#top_up' do
+    let(:card) { Oystercard.new }
+    before { card.top_up(amount) }
+    it 'adds the amount onto the existing balance' do
+      expect(card.balance).to eq amount + balance
+    end
+  end
+
+end
