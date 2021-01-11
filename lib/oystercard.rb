ddfe7ccdd9d58c9f79133979e9a36d807d66b37b
Katy Day <day.katy@gmail.com>
2021-01-11 16:48:30 +0000

    added touch_in method and rspec

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,6 +6,7 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @in_journey 
   end
 
   def top_up(top_up_amount)
@@ -18,7 +19,11 @@ class Oystercard
   end
 
   def in_journey?
-    false
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
   end
 
 end
